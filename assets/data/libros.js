bc428490bb90bb0bbe3d5e542e565fd278512803
Santiago <davidsfigueroar@outlook.es>
2023-04-18 17:47:32 -0500

    estructura datos

---
+++ assets/data/libros.js
@@ -0,0 +1,12 @@
+ const libros = [ 
+    {
+        titulo:"DISEÑO INSTITUCIONAL PARA APLICAR LA GESTIÓN INTEGRADA DEL AGUA Y DEL TERRITORIO GIAT.",
+        fecha:2014,
+        tipo:"Analisis Territorial",
+        subtipo:"Escala Local",
+        url:"url_SM",
+        url_image:"../images/"
+    }
+ ]
+
+ export default libros;
---
+++ assets/data/publicaciones.js
@@ -0,0 +1,12 @@
+const publicaciones = [ 
+    {
+        titulo:"DISEÑO INSTITUCIONAL PARA APLICAR LA GESTIÓN INTEGRADA DEL AGUA Y DEL TERRITORIO GIAT.",
+        fecha:2014,
+        tipo:"Analisis Territorial",
+        subtipo:"Escala Local",
+        url:"url_SM",
+        url_image:"../images/"
+    }
+ ]
+
+ export default libros;
