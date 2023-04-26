25c48a241bbd2ecd1f19f60acba11bf5a43b8d4c
Brayan Ramirez <brayanramirez4546@gmail.com>
2023-04-26 10:23:54 -0500

    add de fecha para filtro

--- assets/data/libros.js
+++ assets/data/libros.js
@@ -1,15 +1,15 @@
  const libros = [ 
     {
-        titulo: "",
-        fecha: 456,
-        escala_regional: true,
+        titulo: "GUÍA PARA LA GESTIÓN AMBIENTAL REGIONAL Y LOCAL",
+        fecha: 1998,
+        escala_regional: false,
         escala_local: false,
         investigacion_ambiental: false,
         manejo_de_agua: false,
         tecnologias_limpias: false,
-        url: "",
-        url_image: "",
-        name: ""
+        url: "https://geogeeks2.maps.arcgis.com/apps/MapJournal/index.html?appid=d38a48ab99ff4d308c322378ff09ca88",
+        url_image: "../assets/images/Libros_img/libro_!.png",
+        name: "libros"
     },
  ]
 
